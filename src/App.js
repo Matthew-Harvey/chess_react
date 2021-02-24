4d6c3d03bbb5ad61038461b767578beede77a66e
Matthew Harvey <matthewtlharvey@gmail.com>
2021-02-24 17:27:31 +0000

    centring icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./css/main.css";
 import "./css/noscript.css";
+import logo from "./img/icon.png";
 
 export default function App() {
   function pracboard() {}
@@ -11,9 +12,18 @@ export default function App() {
         <a href="#">2 Player</a>
         <a href="#">Vs Computer</a>
         <a href="#">Variations</a>
-        <a href="#"></a>
+        <a href="#">Puzzles</a>
       </nav>
       <h2 id="main">Chess</h2>
+      <img
+        src={logo}
+        width="10%"
+        height="10%"
+        style={{
+          display: "block",
+          margin: "auto"
+        }}
+      />
       <h3>test</h3>
     </div>
   );
