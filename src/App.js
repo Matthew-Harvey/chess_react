ed19bf678d06dd17e3ecb7bf1542bfa0f4601cc1
Matthew Harvey <matthewtlharvey@gmail.com>
2021-02-24 12:56:11 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import "./styles.css";
+import "./css/styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Chess</h1>
-      <h2>Play Chess Today!</h2>
+      <h2>Chess</h2>
     </div>
   );
 }
-
-import axios from "axios";
-
-axios
-  .all([
-    axios.get("https://api.github.com/users/mapbox"),
-    axios.get("https://api.github.com/users/phantomjs")
-  ])
-  .then((responseArr) => {
-    //this will be executed only when all requests are complete
-    console.log("Date created: ", responseArr[0].data.created_at);
-    console.log("Date created: ", responseArr[1].data.created_at);
-  });
