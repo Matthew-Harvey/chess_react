24302fb5a7968507a1ab901fadfa8ff0f3ce52b7
Matthew Harvey <matthewtlharvey@gmail.com>
2021-02-24 14:50:14 +0000

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import "./css/styles.css";
+import "./css/main.css";
+import "./css/noscript.css";
 
 export default function App() {
+  function pracboard() {}
+
   return (
     <div className="App">
-      <h2>Chess</h2>
+      <nav>
+        <a href="#">About</a>
+        <a href="#">Works</a>
+        <a href="#">Partners</a>
+        <a href="#">Price</a>
+        <a href="#">Contacts</a>
+      </nav>
+      <h2 id="main">Chess</h2>
+      <h3>test</h3>
     </div>
   );
 }
